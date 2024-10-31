46d10c5a34afa57ff42861f34671e99c4269e8bd
Luna Amanuel <luna.amanuel@sigma.se>
2024-10-31 15:53:03 +0100

    first commit

---
+++ Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/TouchGFXProjects/IDN/TouchGFX/App/app_touchgfx.c \
+ C:/TouchGFXProjects/IDN/TouchGFX/App/app_touchgfx.h
+C:/TouchGFXProjects/IDN/TouchGFX/App/app_touchgfx.h:
---
+++ Debug/Drivers/BSP/Components/ft5336_reg.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/ft5336_reg.o: \
+ C:/TouchGFXProjects/IDN/Drivers/BSP/Components/ft5336/ft5336_reg.c \
+ C:/TouchGFXProjects/IDN/Drivers/BSP/Components/ft5336/ft5336_reg.h
+C:/TouchGFXProjects/IDN/Drivers/BSP/Components/ft5336/ft5336_reg.h:
